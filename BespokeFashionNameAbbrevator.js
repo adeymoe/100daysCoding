c368acc2b0d24a304c6ed80591a367f965b3b6cf
adeymoe <66586650+adeymoe@users.noreply.github.com>
2023-02-24 06:12:32 -0500

    my first push for 100 days challenge

---
+++ BespokeFashionNameAbbrevator.js
@@ -0,0 +1,14 @@
+function abbrevName(name){
+  let abbr = `${name[0]}.${name[name.indexOf(' ')+1]}`
+  return abbr.toUpperCase()
+}
+
+
+
+//testing for the index
+// function abbrevName(name){
+//   // let abbr = `${name[0]} . ${name[name.indexof(' ')+1]}`
+//   console.log(name[0])
+
+// }
+
