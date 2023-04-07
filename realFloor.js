11cc087a9f003857eb89789f644d0dc69fe95c73
adeymoe <66586650+adeymoe@users.noreply.github.com>
2023-04-06 23:45:29 -0400

    adding ternary operation

--- realFloor.js
+++ realFloor.js
@@ -9,3 +9,9 @@ function getRealFloor(n) {
       return 'invalid'
     }
   }
+
+  //how to use ternanry operation for multiple if statement
+
+  function getRealFloor(n) {
+    return n > 13 ? n - 2 : n > 0 ? n - 1 : n;
+  }
