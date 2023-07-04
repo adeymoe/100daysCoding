aac5e20c848fcc364807bd540dea702f0d85f894
adeymoe <66586650+adeymoe@users.noreply.github.com>
2023-07-04 17:41:55 -0400

    fixing case of a string with least changes

---
+++ stringCase.js
@@ -0,0 +1,12 @@
+function solve(s){
+    let lowerC = 0;
+    let upperC = 0;
+    for( let i = 0;i<s.length;i++){
+      if( s[i] == s[i].toUpperCase()){
+        upperC++;
+      }
+      else{
+        lowerC++;
+      }
+    }
+    return lowerC >= upperC ? s.toLowerCase() : s.toUpperCase()
